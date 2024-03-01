0171639b0151234dd4b5e003f2a3f6bd5c4eccd9
vanduyms <vanduyms@gmail.com>
2024-03-01 21:45:51 +0700

    set mode dev and mode production

--- backend/src/middlewares/errorHandlingMiddleware.js
+++ backend/src/middlewares/errorHandlingMiddleware.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from "http-status-codes";
+import { env } from "~/config/environment";
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
@@ -8,6 +9,9 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     message: err.message || StatusCodes[err.statusCode],
     stack: err.stack
   }
+
+
+  if (env.BUILD_MODE !== "dev") delete responseError.stack;
 
   res.status(responseError.statusCode).json(responseError);
 }
