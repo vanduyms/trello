31d6b7e4d8beca242a1de2b399a083ad132cb659
Van Duy <vanduyms@gmail.com>
2024-04-12 16:23:42 +0700

    update config router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Board from "./pages/Boards/_id";
-import BoardHome from "~/pages/Boards";
+import AllBoard from "~/pages/Boards";
 import Profile from "~/pages/Users/_id";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
@@ -26,7 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/board" element={<BoardHome />} />
+        <Route path="/board" element={<AllBoard />} />
         <Route path="/board/:id" element={<Board />} />
         <Route path="/user/:id" element={<Profile />} />
       </Routes>
